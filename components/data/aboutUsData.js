7593381b57f59768eeb279d5286959e9b0599225
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-07-12 17:26:07 +0530

    fix: remove unnecessary text from dv1 field in aboutUsData

--- components/data/aboutUsData.js
+++ components/data/aboutUsData.js
@@ -16,7 +16,7 @@ export const aboutUsData = [
   {
     hv1: "What is",
     hv2: "TEDxBITJaipur",
-    dv1: "TEDxBITJaipur ",
+    dv1: " ",
 
     p: "TEDxBITJaipur is an independently organized TEDx event hosted at Birla Institute of Technology, Mesra – Jaipur Campus. Rooted in TED’s mission of “Ideas Worth Spreading,” TEDxBITJaipur brings together curious minds, bold thinkers, and inspiring voices from all walks of life. Through powerful talks, engaging stories, and meaningful dialogue, we aim to spark change, ignite passion, and build a community driven by ideas. It’s more than an event — it’s a movement, right here on our campus.",
   },
